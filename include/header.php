e3a359b416ee00980211e3e8413cf9b1fa0b7b67
jacop <alfandari.jacopo.stud@itisgalileiroma.it>
2026-03-13 15:41:57 +0100

    creato l'header e fatto tanteee cose

--- include/header.php
+++ include/header.php
@@ -1,1 +1,43 @@
-<?php
+<header class="main-header">
+
+    <div class="header-left">
+        <img src="img/logo.png" class="logo">
+
+        <a href="index.php" class="site-title">
+            Chefly
+        </a>
+    </div>
+
+
+    <div class="header-center">
+        <form action="ricerca.php" method="GET" class="search-form">
+            <input type="text" name="q" placeholder="Cerca ricette..." class="search-bar">
+            <button type="submit" class="search-button">🔍</button>
+        </form>
+    </div>
+
+
+    <div class="header-right">
+
+        <?php if(isset($_SESSION['user_id'])): ?>
+
+            <a href="profilo.php">  <!-- TODO -->
+                <img src="img/fotoProfilo.jpg" class="profile-pic">
+            </a>
+
+            <!--
+       senza spazio: < ?php echo $_SESSION['fotoProfilo']; ?> da ficcare dentro l'src
+    -->
+
+        <?php else: ?>
+
+            <a href="view/login.php" class="login-button">
+                <img src="img/fotoProfilo.jpg" class="user-icon">
+                <span>Accedi</span>
+            </a>
+
+        <?php endif; ?>
+
+    </div>
+
+</header>
--- index.php
+++ index.php
@@ -4,33 +4,32 @@
 <head>
     <meta charset="UTF-8">
     <title>Chefly</title>
+
+    <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/index.css">
+
 </head>
+
 <body>
-<div class="container">
-    <h1>CHEFLY</h1>
-    <p class="subtitle">ricette per tutti</p>
 
-    <?php if (isset($_SESSION['user_id'])): ?>
-        <p>Bentornato in cucina, <b><?php echo htmlspecialchars($_SESSION['username']); ?></b></p>
-        <div class="button-group">
-            <a href="controller/logOutController.php" class="btn btn-logout">Esci</a>
-        </div>
-    <?php else: ?>
-        <p>Sei pronto per una nuova ricetta?</p>
-        <div class="button-group">
-            <a href="view/login.php" class="btn">Login</a>
-            <a href="view/signup.php" class="btn">Registrati</a>
-        </div>
-    <?php endif; ?>
+<?php include "include/header.php"; ?>
 
-    <?php if (isset($_GET['msg'])): ?>
-        <?php if ($_GET['msg']==='successLogin'): ?>
+<?php if (isset($_SESSION['user_id'])): ?>
+    <p>Bentornato in cucina, <b><?php echo htmlspecialchars($_SESSION['username']); ?></b></p>
+
+    <div class="button-group">
+        <a href="controller/logOutController.php" class="btn btn-logout">Esci</a>
+    </div>
+<?php endif; ?>
+
+<?php if (isset($_GET['msg'])): ?>
+    <?php if ($_GET['msg']==='successLogin'): ?>
         <div class="message success">Bentornato nel club! Operazione riuscita.</div>
-        <?php elseif ($_GET['msg']==='successRegister'): ?>
-            <div class="message success">Benvenuto nel club, esegui il login ! .</div>
-        <?php endif; ?>
+
+    <?php elseif ($_GET['msg']==='successRegister'): ?>
+        <div class="message success">Benvenuto nel club, esegui il login!</div>
     <?php endif; ?>
-</div>
+<?php endif; ?>
+
 </body>
 </html>
